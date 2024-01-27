51ae187888fc2beb1a187bfb0d240edec4034b12
Roberto Esteves <contact@robertoesteves.dev>
2024-01-27 15:58:37 -0300

    components: Implement buttons and header bar

---
+++ components/ButtonAnchor.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="group">
+  <button
+    class="text-indigo-300 transition duration-300"
+  >
+    <slot />
+  </button>
+  <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-indigo-300">
+  </span>
+  </div>
+</template>
