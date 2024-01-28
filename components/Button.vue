341f4f93fa16c939a9d5fc26c9067a40eb8db730
maotovisk <me@maot.dev>
2024-01-27 22:45:49 -0300

    components: refactor Button and ButtonOutline to support links whenever there's a href tag.

--- components/Button.vue
+++ components/Button.vue
@@ -1,7 +1,20 @@
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      required: false
+    }
+  }
+}
+</script>
 <template>
-  <button
-    class="inline-flex items-center px-4 py-2 bg-indigo-800 border border-transparent rounded font-semibold text-xs text-white tracking-widest hover:bg-indigo-700 active:bg-indigo-900 focus:outline-none focus:border-indigo-900 focus:ring ring-indigo-300 disabled:opacity-25 transition ease-in-out duration-150"
-  >
+  <NuxtLink v-if="href" :href="href"
+    class="inline-flex items-center px-4 py-2 tracking-widest text-white transition duration-150 ease-in-out bg-indigo-800 border-2 border-transparent border-indigo-700 rounded hover:bg-indigo-700 active:bg-indigo-900 focus:outline-none focus:border-indigo-900 focus:ring ring-indigo-300 disabled:opacity-25">
+    <slot />
+  </NuxtLink>
+  <button v-else
+    class="inline-flex items-center px-4 py-2 tracking-widest text-white transition duration-150 ease-in-out bg-indigo-800 border-2 border-transparent border-indigo-700 rounded hover:bg-indigo-700 active:bg-indigo-900 focus:outline-none focus:border-indigo-900 focus:ring ring-indigo-300 disabled:opacity-25">
     <slot />
   </button>
 </template>
