ad9f9a9c378acf06f4f424210329b7d0ee88374b
Dener <dener.criarbr@gmail.com>
2024-01-28 00:23:47 -0300

    feat: created hero section component

---
+++ components/HeroSection.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class="max-w-screen-xl flex mt-10 justify-around p-20">
+        <div class="max-w-96">
+            <h1 class="text-5xl">Uma carteirinha para todos!</h1>
+            <p class="mt-6 text-2xl ">Para quem tem Autismo diagnósticado e precisa de uma identificação sempre ao alcance.</p>
+            <div class="mt-10 flex gap-6">
+                <ButtonOutline>Criar Conta</ButtonOutline>
+                <Button>Entrar</Button>
+            </div>
+        </div>
+        <div>
+            <img src="./Assets/hero-illustration.svg" alt="Ilustração de 3 garotas jovens segurando um celular para uma selfie, atrás delas um smartphone com a tela de uma carteirinha.">
+        </div>
+    </section>
+</template>
