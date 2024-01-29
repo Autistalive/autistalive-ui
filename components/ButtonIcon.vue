129d879cb17731b6da986bacd03b09623005b971
Roberto Esteves <contact@robertoesteves.dev>
2024-01-28 23:32:42 -0300

    component: Fix component colors

--- components/ButtonIcon.vue
+++ components/ButtonIcon.vue
@@ -1,7 +1,21 @@
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
 <template>
-  <button
-    class="inline-flex items-center p-2 bg-transparent border-2 border-indigo-700 rounded font-semibold tracking-widest hover:bg-indigo-700 hover:text-white active:bg-indigo-900 focus:outline-none focus:border-indigo-900 focus:ring ring-indigo-300 disabled:opacity-25 transition ease-in-out duration-150"
-  >
+  <NuxtLink v-if="href" :href="href"
+    class="inline-flex items-center p-2 bg-transparent border-2 border-accent-primary rounded font-semibold tracking-widest hover:bg-accent-primary hover:text-light-primary active:bg-accent-secondary active:border-accent-secondary focus:outline-none focus:border-accent-primary disabled:opacity-25 transition ease-in-out duration-150">
+    <slot />
+  </NuxtLink>
+  <button v-else
+    class="inline-flex items-center p-2 bg-transparent border-2 border-accent-primary rounded font-semibold tracking-widest hover:bg-accent-primary hover:text-light-primary active:bg-accent-secondary active:border-accent-secondary focus:outline-none focus:border-accent-primary disabled:opacity-25 transition ease-in-out duration-150">
     <slot />
   </button>
 </template>
